82c6e66d84212267167b3407d9f2c24bdbd5fcb7
Tricia Sykes <tlsykes98@gmail.com>
2019-01-14 10:54:58 -0800

    minor changes to style

--- src/component/single.js
+++ src/component/single.js
@@ -7,15 +7,20 @@ class Single extends Component {
     const {match, posts} = this.props
     const id = Number(match.params.id);
     const post = posts.find((post) => post.id === id)
-    const comments = this.props.comments[id] || []
+    const comments = this.props.comments[match.params.id] || []
     const index = this.props.posts.findIndex((post)=> post.id === id)
-
-    return(
-    <div className="single-photo">
-      <Photo post={post} {...this.props} index={index}/>
-      <Comments startAddingComment={this.props.startAddingComment} comments={comments} id={id}/>
-    </div>
-    )
+      if(this.props.loading === true){
+        return <div className="loader">...loading </div>
+      } else if (post) {
+        return (
+          <div className="single-photo">
+            <Photo post={post} {...this.props} index={index}/>
+            <Comments startAddingComment={this.props.startAddingComment} comments={comments} id={id}/>
+          </div>)
+      } else {
+        return <h1>...no post found</h1>
+      }
+    
   }
 }
 
